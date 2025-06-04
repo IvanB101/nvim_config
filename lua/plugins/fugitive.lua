fb48a08e3bcbc014122c38e794ec14cdaa2dc687
IvanB101 <104206321+IvanB101@users.noreply.github.com>
2025-06-04 12:16:14 -0300

    added mappings for merge conflict resolve

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,10 +1,53 @@
+local remap = vim.keymap.set
+local unmap = vim.keymap.del
+local usercmd = vim.api.nvim_create_user_command
+
+---@param lines string[]
+local function diffget(lines, target)
+	local row = vim.api.nvim_win_get_cursor(0)[1]
+
+	local start = row
+	while not lines[start]:find("^<<<<<<<") do
+		start = start - 1
+		if start < 1 then
+			return
+		end
+	end
+	local finish = row
+	while not lines[finish]:find("^>>>>>>>") do
+		finish = finish + 1
+		if finish > #lines then
+			return
+		end
+	end
+
+	local cmd = string.format("%d,%d%s //%d", start, finish, "diffget", target)
+	vim.cmd(cmd)
+end
+
 local config = function()
-	local remap = vim.keymap.set
 	remap("n", "<leader>gt", ":tab Git ", { desc = "Git status" })
 	remap("n", "<leader>gs", "<cmd>tab Git<cr>", { desc = "Git status" })
 	remap("n", "<leader>gv", "<cmd>tab Gdiffsplit<cr>", { desc = "Git diffsplit (with HEAD)" })
 	-- diff operations are vertical by default
 	vim.cmd("set diffopt+=vertical")
+
+	usercmd("GitMergeResolve", function()
+		local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+		vim.cmd("Gvdiffsplit!")
+		remap("n", "<A-h>", function()
+			diffget(lines, 2)
+		end, { desc = "select diff on target branch (left buffer)", buffer = true })
+		remap("n", "<A-l>", function()
+			diffget(lines, 3)
+		end, { desc = "select diff on merge branch (right buffer)", buffer = true })
+	end, { desc = "set buffers and mapping to resolve merge conflics" })
+
+	usercmd("GitMergeResolveEnd", function()
+		vim.cmd("only")
+		unmap("n", "<A-h>", { buffer = true })
+		unmap("n", "<A-l>", { buffer = true })
+	end, { desc = "removes buffers and mapping to resolve merge conflics" })
 end
 
 return {

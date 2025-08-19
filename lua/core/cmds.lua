79a716292acf1affb7fabf5552bf5dae85c868c5
IvanB101 <104206321+IvanB101@users.noreply.github.com>
2025-08-19 19:59:26 +0200

    added some commands

--- lua/core/cmds.lua
+++ lua/core/cmds.lua
@@ -8,3 +8,11 @@ end, { desc = "show current working file" })
 usercmd("Cwf", function()
 	vim.notify(fs.cwf(), vim.log.levels.INFO)
 end, { desc = "show current working file" })
+
+usercmd("Open", function()
+	fs.open(fs.cwf())
+end, { desc = "open current working file with default system app" })
+
+usercmd("Delete", function()
+	fs.delete(fs.cwf())
+end, { desc = "delete current working file" })

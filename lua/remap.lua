7f28fe8b764b028bbec72164f1a343e6085ab449
Ivan Brocas <ivan.brocas@feverup.com@Ivans-MacBook-Pro.local>
2026-03-14 14:46:00 +0100

    changed remaps

--- lua/remap.lua
+++ lua/remap.lua
@@ -3,13 +3,8 @@ local utils = require("utils")
 
 remap("n", "<leader>e", vim.cmd.Ex, { desc = "open netrw" })
 
-remap("n", "<C-s>", "<cmd>w<cr>", { desc = "save current buffer" })
-
 remap({ "n", "v", "o" }, "L", "$", { desc = "go to line end", remap = true })
 remap({ "n", "v", "o" }, "H", "^", { desc = "go to line start", remap = true })
-
-remap("n", "<C-n>", "gt", { desc = "go to next tab" })
-remap("n", "<C-p>", "gT", { desc = "go to previos tab" })
 
 remap("n", "D", vim.diagnostic.open_float, { desc = "open float diagnostic" })
 
@@ -25,13 +20,6 @@ remap({ "n", "v" }, "<leader>y", '"+y', { desc = "yank to clipboard" })
 remap({ "n", "v" }, "<leader>p", '"+p', { desc = "paste from system clipboard" })
 remap({ "n", "v" }, "<leader>P", '"+P', { desc = "paste from system clipboard before cursor" })
 
-remap("n", "<leader>sw", "y/<C-r><C-w><cr>", {
-	desc = "search ocurrencies of word under cursor",
-})
-remap("v", "<Enter>", 'y/<C-r>"<cr>', {
-	desc = "search ocurrencies of marked text ",
-})
-
 remap("v", "c", '"_dP', { desc = "replace marked with clipboard" })
 remap("v", "<leader>c", '"_d"+P', { desc = "replace marked with system clipboard" })
 
@@ -41,6 +29,13 @@ remap("v", "R", 'y:%s/<C-r>"/<C-r>"/g<Left><Left>', { desc = "replace ocurrencie
 remap("n", "U", "<C-r>", { desc = "redo" })
 remap("n", "<C-z>", "<nop>", { desc = "nothing" })
 
+remap("n", "<leader>sw", "y/<C-r><C-w><cr>", {
+	desc = "search ocurrencies of word under cursor",
+})
+remap("v", "<Enter>", 'y/<C-r>"<cr>', {
+	desc = "search ocurrencies of marked text ",
+})
+
 remap("n", "<leader>q", function()
 	utils.quickfix.toggle()
 end, { desc = "toggle quickfix list" })
@@ -49,19 +44,18 @@ remap("n", "[q", "<cmd>cprev<cr>", { desc = "go to previous entry in quickfix li
 
 remap("n", "<leader>vs", "<cmd>vsplit<cr>", { desc = "split window (vertical)" })
 
+remap("n", "<C-n>", "gt", { desc = "go to next tab" })
+remap("n", "<C-p>", "gT", { desc = "go to previos tab" })
+
 remap("n", "<leader>n", "<cmd>tab split<cr>", { desc = "new tab" })
 remap("n", "Q", "<cmd>tabclose<cr>", { desc = "close tab" })
-
-remap({ "n", "v" }, "<leader>w", "g<C-g>", { desc = "count words" })
+remap("n", "<leader>T", "<C-w>T", { desc = "open current split window in new tab" })
 
 remap("n", "<leader>tc", function()
 	utils.editor.replace_word_under_cursor(utils.string.toggle_case)
 end, { desc = "toggle case of word under cursor" })
 
-remap("c", "<C-n>", "<down>", { desc = "line or history down" })
-remap("c", "<C-p>", "<up>", { desc = "line or history up" })
-
 remap("c", "<C-h>", "<s-left>", { desc = "go left one word" })
 remap("c", "<C-l>", "<s-right>", { desc = "go right one word" })
 
-remap("c", "<C-c>", "\\(\\)<left><left>", { desc = "open capture group" })
+remap("c", "<C-g>", "\\(\\)<left><left>", { desc = "open capture group" })

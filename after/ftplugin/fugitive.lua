46795ae03a52510369fb5c42da883d13ce8e16ba
IvanB101 <104206321+IvanB101@users.noreply.github.com>
2026-04-18 16:48:15 +0200

    got fugitive config from latest

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -3,7 +3,20 @@ local bind = function(lhs, rhs, desc)
 	vim.keymap.set("n", lhs, rhs, { remap = true, buffer = true, desc = desc })
 end
 
+local function open_file()
+    local ft, file = vim.api.nvim_get_current_line():match("([^ ]*) ([^ ]*)")
+    if ft == "U" then
+        vim.cmd("tab edit " .. file)
+        vim.cmd("Gdiffsplit!")
+    else
+        vim.cmd("tab edit " .. file)
+    end
+end
+
+
 bind("dd", "O<cmd>Gdiffsplit<cr>", "diffsplit in new tab")
 bind("g?", "<cmd>tab help fugitive<cr>")
 
 remap({ "n", "v" }, "s", "-", { remap = true, buffer = true })
+
+bind("O", open_file)
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,18 +1,16 @@
 local remap = vim.keymap.set
 
 local config = function()
-	-- make diff operations vertical by default
-	vim.cmd("set diffopt+=vertical")
+	vim.cmd("set diffopt+=vertical") -- make diff operations vertical by default
 	local select_conflict_marker = "?^<<<<<<<<CR> V ?^>>>>>>><CR>"
 	remap("v", "m", "<Esc>" .. select_conflict_marker)
-	remap("n", "<leader>dl", select_conflict_marker .. ":diffget //3<CR>")
-	remap("n", "<leader>dh", select_conflict_marker .. ":diffget //2<CR>")
+	remap("n", "<leader>L", select_conflict_marker .. ":diffget //3<CR>")
+	remap("n", "<leader>H", select_conflict_marker .. ":diffget //2<CR>")
 end
 
 return {
 	"tpope/vim-fugitive",
 	keys = {
-		{ "R", "<cmd>G fetch<cr>" },
 		{ "<leader>gt", ":tab Git " },
 		{ "<leader>gs", "<cmd>tab Git<cr>" },
 		{ "<leader>gv", "<cmd>tab Gdiffsplit<cr>" },
